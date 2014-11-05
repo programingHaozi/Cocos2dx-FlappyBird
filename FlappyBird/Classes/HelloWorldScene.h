8bb0b62cf0443c028da44e95995ff610cd2c323b
chenhao <229520281@qq.com>
2014-11-05 17:54:48 +0800

    Cocos2dx-FlappyBird
    Cocos2dx-FlappyBird

---
+++ FlappyBird/Classes/HelloWorldScene.h
@@ -0,0 +1,33 @@
+#ifndef __HELLOWORLD_SCENE_H__
+#define __HELLOWORLD_SCENE_H__
+
+#include "cocos2d.h"
+#include <Box2D/Box2D.h>
+#include "B2Sprite.h"
+class HelloWorld : public cocos2d::Layer
+{
+private:
+    void addbird();
+    void initWorld();
+    Size visibleSize;
+public:
+    // there's no 'id' in cpp, so we recommend returning the class instance pointer
+    static cocos2d::Scene* createScene();
+
+    // Here's a difference. Method 'init' in cocos2d-x returns bool, instead of returning 'id' in cocos2d-iphone
+    virtual bool init();
+    
+    // a selector callback
+    void menuCloseCallback(cocos2d::Ref* pSender);
+    
+    // implement the "static create()" method manually
+    CREATE_FUNC(HelloWorld);
+    
+    virtual void update(float dt);
+    
+    b2World *world;
+   
+    B2Sprite *bird;
+};
+
+#endif // __HELLOWORLD_SCENE_H__
